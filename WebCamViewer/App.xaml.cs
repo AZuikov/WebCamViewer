da8201f08d6c4f564d688d26425e5889e943a12e
Zuykov_A <azuykov1717@gmail.com>
2022-02-19 10:57:54 +0300

    Initial commit

---
+++ WebCamViewer/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WebCamViewer
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+  }
+}
---
+++ WebCamViewer/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WebCamViewer
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
