318f00d586bf14af4581ff50f807a0ca1331367f
Zuykov_A <azuykov1717@gmail.com>
2022-03-12 17:46:49 +0300

    feat: Первичное описание логики функционирования кнопок.

--- WebCamViewer/View/MainWindow.xaml.cs
+++ WebCamViewer/View/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using WebCamViewer.ViewModel;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using WebCamViewer.ViewModel;
 
 namespace WebCamViewer.View
 {
@@ -7,10 +11,31 @@ namespace WebCamViewer.View
     /// </summary>
     public partial class MainWindow
     {
+        public BitmapImage CurrentFrame
+        {
+            get
+            {
+                var currentWebCam = ((AppViewModel)DataContext).CurrentWebCam;
+                return currentWebCam.Frame;
+            }
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new AppViewModel();
+        }
+
+        private void RefreshDeviceListButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataContext = new AppViewModel();
+        }
+
+        private void ConnectToWebCamButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var currentWebCam = ((AppViewModel)DataContext).CurrentWebCam;
+            currentWebCam.StartCapture();
+            
         }
     }
 }
