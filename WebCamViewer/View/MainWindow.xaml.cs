67c032a65ecd12bcea7b8dcb57cd0bb123599722
Zuykov_A <azuykov1717@gmail.com>
2022-04-16 18:37:23 +0300

    feat: Добавлена возможность сохранять кадр в файл PNG.

--- WebCamViewer/View/MainWindow.xaml.cs
+++ WebCamViewer/View/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Media.Imaging;
 using AForge.Video.DirectShow;
+using Microsoft.Win32;
 using WebCamViewer.ViewModel;
 
 namespace WebCamViewer.View
@@ -33,6 +35,28 @@ namespace WebCamViewer.View
             currentWebCam.StopCapture();
         }
 
+
+        private void SaveCurrentFrameButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var bitmapImage = ((AppViewModel) DataContext).CurrentFrame;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG (*.png)|*.png";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+               var filePath = saveFileDialog.FileName;
+                Save(bitmapImage,filePath);
+            }
+        }
         
+        public void Save( BitmapImage image, string filePath)
+        {
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+
+            using (var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+        }
     }
 }
